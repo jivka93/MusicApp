60dddd63118fa1fed0004704c33dd35420857aab
Jivka Ignatova <ignatova.jivka@gmail.com>
2018-06-05 10:34:31 +0300

    adding the Web Api

---
+++ Back-end/MusicApplication.Services/contracts/IArtistService.cs
@@ -0,0 +1,18 @@
+﻿using MusicApplication.Data;
+using System.Collections.Generic;
+
+namespace MusicApplication.Services.contracts
+{
+    public interface IArtistService
+    {
+        IEnumerable<Artist> GetAll();
+
+        Artist GetById(int id);
+
+        Artist CreateArtist(Artist artist);
+
+        Artist UpdateArtist(int id, Artist artist);
+
+        bool DeleteArtist(int id);
+    }
+}
---
+++ Back-end/MusicApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MusicApplication.WebApiApplication" Language="C#" %>
