96c15543af12cee09e38faa67d80ba0657d15bdd
Jivka Ignatova <ignatova.jivka@gmail.com>
2018-06-06 19:25:32 +0300

    registration changes

--- app/js/controllers/RegisterUserController.js
+++ app/js/controllers/RegisterUserController.js
@@ -2,10 +2,25 @@
 
 musicApp.controller('RegisterUserController', function RegisterUserController($scope) {
 
-    $scope.submit = function() {
-        var username = $scope.Username;
-        var password = $scope.Password;
-        var repeatPassword = $scope.RepeatPassword;
+    $scope.showPasswordError = false;
+
+    $scope.registerUser = function() {
+
+        var password = $scope.user.password;
+        var repeatPassword = $scope.user.passwordVerify;
+
+        if (password !== repeatPassword) {
+            $scope.showPasswordError = true;
+        } else {
+
+            $scope.showPasswordError = false;
+
+            var user = {
+                username: $scope.user.username,
+                password: $scope.user.password
+            };
+
+        }
 
     };
 });
