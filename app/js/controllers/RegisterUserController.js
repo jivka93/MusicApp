d0a3df9e00244b890dfc27cda73a2822589b2946
Jivka Ignatova <ignatova.jivka@gmail.com>
2018-06-06 16:12:10 +0300

    RegisterModal

---
+++ app/js/controllers/RegisterUserController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+musicApp.controller('RegisterUserController', function RegisterUserController($scope) {
+
+    $scope.submit = function() {
+        var username = $scope.Username;
+        var password = $scope.Password;
+        var repeatPassword = $scope.RepeatPassword;
+
+    };
+});
